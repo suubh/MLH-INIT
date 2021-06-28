e7522236f2ae16774596cd6eb69fbffa45b32e17
suubh <subhdec99@gmail.com>
2021-06-29 03:08:45 +0530

    Added

---
+++ BeFit/app.js
@@ -0,0 +1,62 @@
+
+const auth_link = "https://www.strava.com/oauth/token"
+
+function getActivites(res){
+
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    fetch(activities_link)
+        .then((res) => res.json())
+        .then(function (data){
+
+            var map = L.map('map').setView([38.895417, -77.033616], 11);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            for(var x=0; x<data.length; x++){
+
+                console.log(data[x].map.summary_polyline)
+                var coordinates = L.Polyline.fromEncoded(data[x].map.summary_polyline).getLatLngs()
+                console.log(coordinates)
+
+                L.polyline(
+
+                    coordinates,
+                    {
+                        color:"green",
+                        weight:5,
+                        opacity:.7,
+                        lineJoin:'round'
+                    }
+
+                ).addTo(map)
+            }
+
+        }
+        )
+}
+
+    
+function reAuthorize() {
+    fetch(auth_link, {
+        method: 'post',
+
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+
+            client_id: 'xxxxx', //For Security reasons
+            client_secret: 'xxxxx',
+            refresh_token: 'xxxxx',
+            grant_type: 'refresh_token'
+
+        })
+
+    }).then(res => res.json())
+        .then(res => getActivites(res))
+}
+
+reAuthorize()
