d81e94d0869f2bf8546ed980846a5618f201c89a
suubh <subhdec99@gmail.com>
2021-06-29 15:33:17 +0530

    Added

--- listSort.py
+++ listSort.py
@@ -1,1 +1,26 @@
+import requests 
+import random
+response = requests.get ("https://mlh-events.now.sh/na-2019")
+#print(response.json())
+
+#Now get the name and the date of the hackathon in a list 
+list_res=response.json()
+name=[]   #List of Hackathons name
+date=[]   #List of Hackathons date
+for i in range(0,len(list_res)):
+    dict=list_res[i]
+    val_list=dict.values()
+    val_list=list(val_list)
+    name.append(val_list[0])
+    date.append(val_list[2])
+
+#Sorting Name
+random.shuffle(name)
+print(name)
+sorted_name=name.sort()
+print(sorted_name)
+
+
+
+
 
