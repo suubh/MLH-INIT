d968fd8a38a08cdabbd4700912c59184d467d976
suubh <subhdec99@gmail.com>
2021-07-01 11:13:26 +0530

    Random

---
+++ random_num_generator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <time.h>
+#include<stdlib.h>
+usin namespace std;
+int main(){
+	srand(time(0))
+	for(int i=0;i<10;i++
+	{
+		cout << rand() <<endl;
+	}
+	return 0;
+}
+
+
