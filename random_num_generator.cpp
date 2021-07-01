2d4473a6a0a72a25f92605b268a3c044ea221554
suubh <subhdec99@gmail.com>
2021-07-01 11:18:34 +0530

    Checked

--- random_num_generator.cpp
+++ random_num_generator.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include <time.h>
 #include<stdlib.h>
-usin namespace std;
+using namespace std;
 int main(){
-	srand(time(0))
-	for(int i=0;i<10;i++
+	srand(time(0));
+	for(int i=0;i<10;i++)
 	{
 		cout << rand() <<endl;
 	}
